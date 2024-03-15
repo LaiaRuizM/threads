f6b4e5ec88858de0108516127414648b2eee15dd
LaiaRuizM <laia.ruizm@hotmail.com>
2024-03-15 13:12:28 -0700

    feat: update feed's style

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,18 +3,17 @@
 const Feed = () => {
   return (
     <div className="container mx-auto w-[600px]">
-      <div>
+      <div className="flex">
         <img
+          className="w-12 rounded-full object-cover"
           src="https://media.licdn.com/dms/image/D4D03AQFP5XYJUdkTsQ/profile-displayphoto-shrink_800_800/0/1670778116949?e=1715817600&v=beta&t=QcRpQ80IauDBs75MWuTLCScZd1fsBmMtiMbW1o_4ya0"
           alt=""
         />
-        <div>
-          <div>
-            <div>
-              <strong>Laia</strong>
+        <div className="w-full p-2">
+          <div className="flex justify-between gap-2">
+            <strong>Laia</strong>
 
-              <div>3hrs ago</div>
-            </div>
+            <div>3hrs ago</div>
           </div>
         </div>
       </div>
