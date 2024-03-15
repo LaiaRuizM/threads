65b2f0f7a860a9c12ea21c4ae0790b56c1cfc932
LaiaRuizM <laia.ruizm@hotmail.com>
2024-03-15 12:45:48 -0700

    feat: add structure to feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,7 +3,21 @@
 const Feed = () => {
   return (
     <div className="container mx-auto w-[600px]">
-      <p className="text-3xl font-bold underline text-red-800">FEED</p>
+      <div>
+        <img
+          src="https://media.licdn.com/dms/image/D4D03AQFP5XYJUdkTsQ/profile-displayphoto-shrink_800_800/0/1670778116949?e=1715817600&v=beta&t=QcRpQ80IauDBs75MWuTLCScZd1fsBmMtiMbW1o_4ya0"
+          alt=""
+        />
+        <div>
+          <div>
+            <div>
+              <strong>Laia</strong>
+
+              <div>3hrs ago</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
