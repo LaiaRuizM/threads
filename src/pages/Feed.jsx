28fff3850ecaf8d761a621e746ec0c6d732f6e9e
LaiaRuizM <laia.ruizm@hotmail.com>
2024-03-19 12:17:31 -0700

    feat: update style to feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,19 +3,19 @@ import { MoreHorizontal } from "react-feather";
 
 const Feed = () => {
   return (
-    <div className="container mx-auto w-[600px]">
-      <div className="flex">
+    <div className="container mx-auto max-w-[600px]">
+      <div className="flex p-4">
         <img
           className="w-12 rounded-full object-cover"
           src="https://media.licdn.com/dms/image/D4D03AQFP5XYJUdkTsQ/profile-displayphoto-shrink_800_800/0/1670778116949?e=1715817600&v=beta&t=QcRpQ80IauDBs75MWuTLCScZd1fsBmMtiMbW1o_4ya0"
           alt=""
         />
-        <div className="w-full p-2">
+        <div className="w-full px-2">
           <div className="flex justify-between gap-2">
             <strong>Laia</strong>
 
-            <div>
-              <p>3hrs ago</p>
+            <div className="flex justify-between gap-2">
+              <p className="text-[rgba(97,97,97,1)]">3hrs ago</p>
               <MoreHorizontal />
             </div>
           </div>
