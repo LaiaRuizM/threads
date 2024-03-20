8db0f18125d90e433016707b087c3293c0532431
LaiaRuizM <laia.ruizm@hotmail.com>
2024-03-20 13:10:53 -0700

    feat: configure database

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,10 +1,13 @@
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
 
 const client = new Client();
 
-const VITE_ENDPOINT = import.meta.env.VITE_ENDPOINT;
-const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+export const VITE_ENDPOINT = import.meta.env.VITE_ENDPOINT;
+export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+export const DEV_DB_ID = import.meta.env.VITE_DB_ID;
+export const COLLECTION_ID_THREADS = import.meta.env.VITE_COLLECTION_ID_THREADS;
 
 client.setEndpoint(VITE_ENDPOINT).setProject(PROJECT_ID);
 
+export const database = new Databases(client);
 export default client;
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+// import { useState } from "react";
 import Thread from "../components/Thread";
+import { database, DEV_DB_ID, COLLECTION_ID_THREADS } from "../appwriteConfig";
 
 const Feed = () => {
+  //   const [threads, setThreads] = useState();
+
+  useEffect(() => {
+    getThreads();
+  }, []);
+
+  const getThreads = async () => {
+    const response = await database.listDocuments(
+      DEV_DB_ID,
+      COLLECTION_ID_THREADS
+    );
+    console.log("response:", response);
+  };
+
   return (
     <div className="container mx-auto max-w-[600px]">
       <Thread />
