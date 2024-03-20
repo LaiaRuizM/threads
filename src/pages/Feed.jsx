f1489cb5b4c47466760c144a33bfc66bb411892c
LaiaRuizM <laia.ruizm@hotmail.com>
2024-03-19 17:44:56 -0700

    feat: update style and add icons section

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,16 +1,23 @@
 // import React from 'react';
-import { MoreHorizontal } from "react-feather";
+import {
+  MoreHorizontal,
+  Heart,
+  MessageCircle,
+  Repeat,
+  Send,
+} from "react-feather";
 
 const Feed = () => {
   return (
     <div className="container mx-auto max-w-[600px]">
       <div className="flex p-4">
         <img
-          className="w-12 rounded-full object-cover"
+          className="w-12 h-12 rounded-full object-cover"
           src="https://media.licdn.com/dms/image/D4D03AQFP5XYJUdkTsQ/profile-displayphoto-shrink_800_800/0/1670778116949?e=1715817600&v=beta&t=QcRpQ80IauDBs75MWuTLCScZd1fsBmMtiMbW1o_4ya0"
           alt=""
         />
         <div className="w-full px-2">
+          {/* Thread header*/}
           <div className="flex justify-between gap-2">
             <strong>Laia</strong>
 
@@ -18,6 +25,21 @@ const Feed = () => {
               <p className="text-[rgba(97,97,97,1)]">3hrs ago</p>
               <MoreHorizontal />
             </div>
+          </div>
+
+          {/* Thread body*/}
+          <div className="py-4">
+            <span>
+              Zuckerberg claims that this app has already 30M users. If that is
+              true it is fkin impressive.
+            </span>
+          </div>
+
+          <div className="flex gap-4 py-4">
+            <Heart size={22} />
+            <MessageCircle size={22} />
+            <Repeat size={22} />
+            <Send size={22} />
           </div>
         </div>
       </div>
