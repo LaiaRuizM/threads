f3804429f95a74c86a64a81860c2a46cb0a3b7cc
LaiaRuizM <laia.ruizm@hotmail.com>
2024-03-19 19:24:38 -0700

    chore: install appwrite, import Client and hide setProject and setEndpoint

---
+++ src/appwriteConfig.js
@@ -0,0 +1,10 @@
+import { Client } from "appwrite";
+
+const client = new Client();
+
+const VITE_ENDPOINT = import.meta.env.VITE_ENDPOINT;
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+
+client.setEndpoint(VITE_ENDPOINT).setProject(PROJECT_ID);
+
+export default client;
