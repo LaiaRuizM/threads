ea63c4ae4941f611b15c3be48bcf89ba1b56cde7
LaiaRuizM <laia.ruizm@hotmail.com>
2024-04-26 18:38:11 -0700

    feat: add AuthContext to do Authentication

---
+++ src/context/AuthContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = children => {
+  const [loading, setLoading] = useState(true);
+  console.log(setLoading);
+  const contextData = {};
+  return (
+    <AuthContext.Provider values={contextData}>
+      {loading ? <p>Loading...</p> : children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
