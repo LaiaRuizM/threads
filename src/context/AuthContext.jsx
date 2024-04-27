b7245531f496d47f4e14d1d5198496f498b01f8c
LaiaRuizM <laia.ruizm@hotmail.com>
2024-04-26 18:56:13 -0700

    feat: fix problem of destructure children from the props object and props validation

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,16 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 const AuthContext = createContext();
 
-export const AuthProvider = children => {
+export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  console.log(setLoading);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   const contextData = {};
   return (
     <AuthContext.Provider value={contextData}>
       {loading ? <p>Loading...</p> : children}
     </AuthContext.Provider>
   );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthContext;
