429053ccff8980589f3a730a2d9f05be72159781
LaiaRuizM <laia.ruizm@hotmail.com>
2024-04-28 13:10:33 -0700

    feat: add form to Login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+// import { useRef } from "react";
+
+const Login = () => {
+  //   const loginForm = useRef(null);
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="">Email:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="">Password:</label>
+          <input type="password" name="password" />
+        </div>
+        <div>
+          <input type="submit" name="Login" />
+        </div>
+      </form>
+      <p>
+        Don`&apos`t have an account? <Link to="/register" />
+      </p>
+    </div>
+  );
+};
+
+export default Login;
