fcea000a7db7599ef6b694e19e29e890ba4efa73
LaiaRuizM <laia.ruizm@hotmail.com>
2024-04-30 14:13:52 -0700

    feat: update profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,23 +5,23 @@ import { Query } from "appwrite";
 
 const Profile = () => {
   const [threads, setThreads] = useState([]);
-  const params = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
-    console.log("params:", params);
-    // getThreads();
+    getThreads();
   }, []);
 
-  //   const getThreads = async () => {
-  //     const response = await database.listDocuments(
-  //       DEV_DB_ID,
-  //       COLLECTION_ID_THREADS,
-  //       [Query.orderDesc("$createdAt"), Query.equal("owner_id")]
-  //     );
-  //     console.log("response feed:", response);
-  //     setThreads(response.documents);
-  //     console.log(response.documents);
-  //   };
+  const getThreads = async () => {
+    const response = await database.listDocuments(
+      DEV_DB_ID,
+      COLLECTION_ID_THREADS,
+      [Query.orderDesc("$createdAt"), Query.equal("owner_id", id)]
+    );
+    console.log("response profile:", response);
+    setThreads(response.documents);
+    console.log(response.documents);
+    console.log(threads);
+  };
 
   return <div>Profile</div>;
 };
