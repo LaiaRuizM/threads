f605c69869eefd6ef4598b1b7c3f713709f13538
LaiaRuizM <laia.ruizm@hotmail.com>
2024-04-30 13:48:33 -0700

    feat: add Profile to App.jsx and its Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/Header";
 import Feed from "./pages/feed";
 import Login from "./pages/Login";
-import Header from "./components/Header";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Feed />} />
+          <Route path="/profile/:id" element={<Profile />} />
         </Routes>
       </AuthProvider>
     </Router>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,27 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { database, DEV_DB_ID, COLLECTION_ID_THREADS } from "../appwriteConfig";
+import { Query } from "appwrite";
 
 const Profile = () => {
   const [threads, setThreads] = useState([]);
-  useEffect(() => {});
+  const params = useParams();
+
+  useEffect(() => {
+    getThreads();
+  });
+
+  //   const getThreads = async () => {
+  //     const response = await database.listDocuments(
+  //       DEV_DB_ID,
+  //       COLLECTION_ID_THREADS,
+  //       [Query.orderDesc("$createdAt"), Query.equal("owner_id")]
+  //     );
+  //     console.log("response feed:", response);
+  //     setThreads(response.documents);
+  //     console.log(response.documents);
+  //   };
+
   return <div>Profile</div>;
 };
 
