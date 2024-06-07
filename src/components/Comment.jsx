f06893d9e8865bbc63c613c4b743718d3feae52a
LaiaRuizM <laia.ruizm@hotmail.com>
2024-06-07 16:29:41 -0700

    feat: render out the likes and take out replies from comments for now

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,19 +1,21 @@
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import // MoreHorizontal,
-// Heart,
-// MessageCircle,
-// Repeat,
-// Send,
-//Trash2,
-"react-feather";
+import {
+  // MoreHorizontal,
+  Heart,
+  // MessageCircle,
+  // Repeat,
+  // Send,
+  //Trash2,
+} from "react-feather";
 import {
   functions,
   database,
   DEV_DB_ID,
   COLLECTION_ID_PROFILES,
+  COLLECTION_ID_COMMENTS,
 } from "../appwriteConfig";
+import { useAuth } from "../context/AuthContext";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import ReactTimeAgo from "react-time-ago";
@@ -22,6 +24,9 @@ TimeAgo.addLocale(en);
 const Comment = ({ comment }) => {
   const [loading, setLoading] = useState(true);
   const [owner, setOwner] = useState(null);
+  const [commentInstance, setCommentInstance] = useState(comment);
+  const { user } = useAuth();
+
   useEffect(() => {
     getUserInfo();
   }, []);
@@ -50,6 +55,33 @@ const Comment = ({ comment }) => {
     setLoading(false);
   };
 
+  const toggleLike = async () => {
+    console.log("Liked toggled:", comment);
+
+    const users_who_liked = comment.users_who_liked;
+
+    if (users_who_liked.includes(user.$id)) {
+      const index = users_who_liked.indexOf(user.$id);
+      users_who_liked.splice(index, 1);
+    } else {
+      users_who_liked.push(user.$id); //adding to the array
+    }
+
+    const payload = {
+      users_who_liked: users_who_liked,
+      likes: users_who_liked.length,
+    };
+
+    const response = await database.updateDocument(
+      DEV_DB_ID,
+      COLLECTION_ID_COMMENTS,
+      comment.$id,
+      payload
+    );
+
+    setCommentInstance(response);
+  };
+
   if (loading) return;
 
   return (
@@ -63,7 +95,7 @@ const Comment = ({ comment }) => {
       <div className="w-full px-2 pb-4 border-b border-[rgba(97,97,97,1)]">
         {/* Thread Header */}
         <div className="flex justify-between gap-2">
-          {/* <strong> FIX IT! {owner.name} </strong> */}
+          <strong> FIX IT comments! {owner.name} </strong>
 
           <div className="flex justify-between gap-2 items-center cursor-pointer">
             <p className="text-[rgba(97,97,97,1)]">
@@ -79,42 +111,38 @@ const Comment = ({ comment }) => {
         </div>
 
         {/* Thread Body */}
-        <Link to={`/thread/${comment.$id}`}>
-          <div className="py-4 text-white" style={{ whiteSpace: "pre-wrap" }}>
-            {comment.body}
-            {/* {comment.image && (
+        <div className="py-4 text-white" style={{ whiteSpace: "pre-wrap" }}>
+          {comment.body}
+          {/* {comment.image && (
               <img
                 className="object-cover border border-[rgba(49,49,50,1)] rounded-md"
                 src={comment.image}
               />
             )} */}
-          </div>
-        </Link>
+        </div>
 
-        {/* <div className="flex gap-4 py-4">
+        <div className="flex gap-4 py-4">
           <Heart
             onClick={toggleLike}
             size={22}
             className="cursor-pointer"
             color={
-              threadInstance.users_who_liked.includes(user.$id)
+              commentInstance?.users_who_liked.includes(user.$id)
                 ? "#ff0000"
                 : "#fff"
             }
           />
-          <Link to={`/thread/${comment.$id}`}>
-            <MessageCircle size={22} color={"#fff"} />
-          </Link>
-          <Repeat size={22} />
-          <Send size={22} />
-        </div> */}
+          {/* <MessageCircle size={22} color={"#fff"} /> */}
+          {/* <Repeat size={22} />
+          <Send size={22} /> */}
+        </div>
 
         <div className="flex gap-4">
-          <p className="text-[rgba(97,97,97,1)]">16 Replies</p>
-          <p className="text-[rgba(97,97,97,1)]">·</p>
-          {/* <p className="text-[rgba(97,97,97,1)]">
-            {threadInstance.likes} Likes
-          </p> */}
+          {/* <p className="text-[rgba(97,97,97,1)]">16 Replies</p>
+          <p className="text-[rgba(97,97,97,1)]">·</p> */}
+          <p className="text-[rgba(97,97,97,1)]">
+            {commentInstance?.likes} Likes
+          </p>
         </div>
       </div>
     </div>
