90446231a45db195f96ba840ab3312d807152f80
LaiaRuizM <laia.ruizm@hotmail.com>
2024-06-28 14:13:06 -0700

    chore: fix error - don't showing following posts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
 
 Header.propTypes = {
   profile_pic: PropTypes.func,
-  username: PropTypes.string.isRequired,
+  username: PropTypes.string,
 };
 
 export default Header;
